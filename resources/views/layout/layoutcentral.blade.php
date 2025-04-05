071648c8d529f207810499b4ab5597f98bcb9fbf
Pachara2004 <pachara000004@gmail.com>
2025-04-05 16:21:07 +0700

    stly(layoutcentral): เพิ่ม CSS ของฟังก์ชั่นแก้ไข

--- resources/views/layout/layoutcentral.blade.php
+++ resources/views/layout/layoutcentral.blade.php
@@ -191,6 +191,51 @@
         .btn-success:hover {
             background-color: #45a049;
         }
+        .edit-button {
+            background-color: #007bff;
+            color: white;
+            padding: 5px 10px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        .modal {
+            display: none;
+            position: fixed;
+            z-index: 1;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgb(0, 0, 0);
+            background-color: rgba(0, 0, 0, 0.4);
+        }
+
+        .modal-content {
+            background-color: #fefefe;
+            margin: 15% auto;
+            padding: 20px;
+            border: 1px solid #888;
+            width: 80%;
+            border-radius: 15px;
+            box-shadow: #45a049;
+        }
+
+        .close {
+            color: #aaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
+
+        .close:hover,
+        .close:focus {
+            color: black;
+            text-decoration: none;
+            cursor: pointer;
+        }
     </style>
 </head>
 
