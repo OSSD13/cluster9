636914d12dd0737ac28309dc3928402887d365f4
Pachara2004 <pachara000004@gmail.com>
2025-04-06 10:43:35 +0700

    update

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL; // เพิ่ม namespace URL
 
 define('LARAVEL_START', microtime(true));
 
@@ -16,5 +17,17 @@ require __DIR__.'/vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/bootstrap/app.php';
+
+// Proxy Configuration
+$proxy_url    = getenv('PROXY_URL');
+$proxy_schema = getenv('PROXY_SCHEMA');
+
+if (!empty($proxy_url)) {
+    URL::forceRootUrl($proxy_url);
+}
+
+if (!empty($proxy_schema)) {
+    URL::forceSchema($proxy_schema);
+}
 
 $app->handleRequest(Request::capture());
